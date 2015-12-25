7d373698d661024f902bfac23d9bd9d4b8771ba0
knolegsb <seon2you@hotmail.com>
2015-12-25 01:31:47 -0800

    web api

---
+++ VideoWebApiUdemy/Controllers/VideoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VideoWebApiUdemy.Models;
+
+namespace VideoWebApiUdemy.Controllers
+{
+    public class VideoController : ApiController
+    {
+        VideoDb _db = new VideoDb();
+        // GET: api/Video
+        public IEnumerable<Video> Get()
+        {
+            return _db.Videos;
+        }
+
+        // GET: api/Video/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Video
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Video/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Video/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
